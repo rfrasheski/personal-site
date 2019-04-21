80c385e645675844959245c2615baa780259f43e
rfrasheski <rfrasheski@gmail.com>
2019-04-20 19:32:59 -0700

    google analytics id

--- config/website.js
+++ config/website.js
@@ -17,7 +17,7 @@ module.exports = {
   userTwitter: '@rfrasheski', // Twitter Username
   ogSiteName: 'rfrasheski', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  googleAnalyticsID: 'UA-138734618-1',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
